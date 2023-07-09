79c5fd39d18695b7f62d8856442ec3c2e931ffe2
Kamil Perczyński <43953005+kamil-perczynski@users.noreply.github.com>
2023-07-09 22:14:25 +0200

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,11 +1,11 @@
 if ($env:APPSETTINGS_XML) {
-  echo "Overriding appsettings.xml with environent variable"
-  echo $env:APPSETTINGS_XML | Out-File -FilePath .\appsettings.xml
+  echo "Overriding appsettings.xml with environent variable: $env:APPSETTINGS_XML"
+  $env:APPSETTINGS_XML | Out-File -FilePath ".\appsettings.xml" -Encoding UTF8
 }
 
 if ($env:CONNECTIONSTRINGS_XML) {
-  echo "Overriding connectionstrings.xml with environent variable"
-  echo $env:CONNECTIONSTRINGS_XML | Out-File -FilePath .\connectionstrings.xml
+  echo "Overriding connectionstrings.xml with environent variable: $env:CONNECTIONSTRINGS_XML"
+  $env:CONNECTIONSTRINGS_XML | Out-File -FilePath ".\connectionstrings.xml" -Encoding UTF8
 }
 
 C:\ServiceMonitor.exe w3svc
